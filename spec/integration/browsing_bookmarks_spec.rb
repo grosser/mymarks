c1835ef4b4479a3812bad66cd42dadc37f61e2c3
grosser <grosser.michael@gmail.com>
2011-11-25 12:16:13 -0800

    clicking on folder

--- spec/integration/browsing_bookmarks_spec.rb
+++ spec/integration/browsing_bookmarks_spec.rb
@@ -5,16 +5,27 @@ describe "browsing bookmarks", :js => true do
     page.find(css).click
   end
 
-  before do
-    MyMarks::Parser.stub!(:get_html).and_return File.read('spec/fixtures/bookmarks.txt')
-  end
-
-  it "loads my bookmarks" do
+  def login
     visit "/"
     page.should have_content('MyMarks')
     fill_in 'username', :with => 'mymarks_test'
     fill_in 'password', :with => 'mymarks_test_pw'
     click_css '#login input[type=submit]'
-    wait_until{ page.has_content? 'Google' }
+  end
+
+  before do
+    MyMarks::Parser.stub!(:get_html).and_return File.read('spec/fixtures/bookmarks.txt')
+    login
+  end
+
+  it "loads my bookmarks" do
+    wait_until{ page.has_content? 'Google' } # entry
+    wait_until{ page.has_content? 'F1' } # folder
+    page.should_not have_content 'Firefox' # 'place:' bookmark that should be hidden
+  end
+
+  it "allows me to click into folders" do
+    click_css "a[data-type=folder]"
+    wait_until{ page.has_content? 'Yahoo' } # entry in F1
   end
 end
