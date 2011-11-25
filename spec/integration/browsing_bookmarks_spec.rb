4177b7571d7146d27d14b5984ac8cc2de2f6697e
grosser <grosser.michael@gmail.com>
2011-11-25 11:58:11 -0800

    a browser

--- spec/integration/browsing_bookmarks_spec.rb
+++ spec/integration/browsing_bookmarks_spec.rb
@@ -1,8 +1,16 @@
 require 'spec_helper'
 
-describe "browsing bookmarks" do
-  it "xxx", :js => true do
+describe "browsing bookmarks", :js => true do
+  def click_css(css)
+    page.find(css).click
+  end
+
+  it "loads my bookmarks" do
     visit "/"
     page.should have_content('MyMarks')
+    fill_in 'username', :with => 'mymarks_test'
+    fill_in 'password', :with => 'mymarks_test_pw'
+    click_css '#login input[type=submit]'
+    wait_until{ page.has_content? 'Google' }
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,5 +8,6 @@ require 'capybara/dsl'
 Capybara.app = Sinatra::Application.new
 
 RSpec.configure do |config|
+  Capybara.current_driver = :selenium
   config.include Capybara::DSL
 end
